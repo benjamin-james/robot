20f24f69232a27a9001b21817d854b9f549a5ca6
pbandj24 <2015376@sluh.org>
2015-03-04 15:30:51 -0600

    Update demo.c

--- src/demo.c
+++ src/demo.c
@@ -10,13 +10,13 @@ int main(int argc, char **argv)
 	int i;
 	do
 	{
-		printf("%f\n\narm\t\tangle\n",error);
+		printf("error:\t%f\n\narm\t\tangle\n",error);
 		for(i = 0; i < NUM_ARMS; i++)
 		{
 			printf("%f\t%f\n",arms[i],angles[i]);
 		}
 		
-	}while((error = calculate_position_times(NUM_ARMS,arms,angles,1,8,8)) > precision);
+	}while((error = calculate_position_times(NUM_ARMS,arms,angles,1,8,8)) > precision);	//loop 1 time to position (8,8), but do that until precision is reached
 	printf("%f\n\n",error);
 	mat3_t end = calc_end(NUM_ARMS,arms,angles);
 	mat3_print(&end);
