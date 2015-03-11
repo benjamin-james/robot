881cfe9de1d1a739621d652bf37b9453ff93fd7d
pbandj24 <2015376@sluh.org>
2015-03-11 14:57:15 -0500

    Updated and cleaned up robot.c

--- src/robot.c
+++ src/robot.c
@@ -10,16 +10,22 @@
 //returns the error from the intended target, or -1 on failure
 //move has two parameters:	(int) 		the index of the angle to be changed
 //				(double)	the change in angle measure in radians
-double moveTowards(int size, double *joints, double *angles, int times, double goal_x, double goal_y, void (*move)(int,double))
+double moveTowards(int size, double *joints, double *angles, int times, double goal_x, double goal_y, void (*move)(int, double))
 {
-	if(!joints || !angles || size < 1 || !move) return -1;
+	if (!joints || !angles || size < 1 || !move)
+		return -1;
 	double *prev = malloc(sizeof(double)*size);
-	memcpy(prev,angles,size);
-	double result = calculate_position_times(size,joints,angles,times,goal_x,goal_y);
+	memcpy(prev, angles, size);
+	double result = calculate_position_times(size, joints, angles, times, goal_x, goal_y);
 	int i;
-	for(i = 0; result >= 0.0 && i < size; i++)
-	{
-		move(i,angles[i]-prev[i]);
+	double d = 0;
+	for (i = 0; result >= 0.0 && i < size; i++) {
+		d = angles[i]-prev[i];
+		while (d >= TAU / 2)
+			d -= TAU;
+		while (d < -TAU / 2)
+			d += TAU;
+		move(i, d);
 	}
 	free(prev);
 	return result;
@@ -36,21 +42,23 @@ void step(double *angle, mat3_t prev, mat3_t tool, vec2_t goal)
 	vec2_t toolP = mat3_getPosition(tool);
 	vec2_t a = {toolP.x - prevP.x, toolP.y - prevP.y};
 	vec2_t b = {goal.x - prevP.x, goal.y - prevP.y};
-	*angle += atan2(b.y,b.x) - atan2(a.y,a.x);
-	while(*angle > TAU) *angle -= TAU;
-	while(*angle < 0) *angle += TAU;
+	*angle += atan2(b.y, b.x) - atan2(a.y, a.x);
+	while (*angle >= TAU / 2) 
+		*angle -= TAU;
+	while (*angle < -TAU / 2) 
+		*angle += TAU;
 }
 //iterates "iterations" times, returns error
 double calculate_position_times(int size, double *joints, double *angles, int iterations, double goal_x, double goal_y)
 {
-	if(!joints || !angles) return -1;
-	mat3_t tool = calc_end(size,joints,angles);
+	if (!joints || !angles)
+		return -1;
+	mat3_t tool = calc_end(size, joints, angles);
 	vec2_t goal = {goal_x, goal_y};
 	int i;
 	double err = 0;
-	for(i = 0; i < iterations; i++)
-	{
-		tool = adjust(size,joints,angles,tool,goal);//the computation
+	for (i = 0; i < iterations; i++) {
+		tool = adjust(size, joints, angles, tool, goal);//the computation
 	}
 	vec2_t toolP = mat3_getPosition(tool);		//extract a coordinate from matrix
 	err = (toolP.x - goal_x)*(toolP.x - goal_x) + (toolP.y - goal_y)*(toolP.y - goal_y); //calculate distance left to travel robot arm
@@ -59,15 +67,15 @@ double calculate_position_times(int size, double *joints, double *angles, int it
 //iterates adjustion until error is sufficiently small
 int calculate_position(int size, double *joints, double *angles, double error, double goal_x, double goal_y)
 {
-	if(!joints || !angles) return -1;
+	if (!joints || !angles)
+		return -1;
 	error *= error; //error squared
-	mat3_t tool = calc_end(size,joints,angles);
+	mat3_t tool = calc_end(size, joints, angles);
 	vec2_t goal = {goal_x, goal_y};
 	int i;
 	double err = error+1;
-	for(i = 0; err >= error; i++)
-	{
-		tool = adjust(size,joints,angles,tool,goal);//the computation
+	for (i = 0; err >= error; i++) {
+		tool = adjust(size, joints, angles, tool, goal);//the computation
 		vec2_t toolP = mat3_getPosition(tool);		//extract a coordinate from matrix
 		err = (toolP.x - goal_x)*(toolP.x - goal_x) + (toolP.y - goal_y)*(toolP.y - goal_y); //calculate distance left to travel robot arm
 	}
@@ -79,29 +87,27 @@ mat3_t adjust(int size, double *joints, double *angles, mat3_t tool, vec2_t goal
 {
 	mat3_t prev = mat3_identity();
 	int i;
-	for(i = 0; i < size; i++)
-	{
-		mat3_t next = mat3_multiply(prev,calc_joint(joints[i],angles[i]));
-		step(angles+i,prev,tool,goal);
-		prev = mat3_multiply(prev,calc_joint(joints[i],angles[i]));
-		tool = mat3_multiply(mat3_multiply(prev,mat3_inverse(next)),tool);
+	for (i = 0; i < size; i++) {
+		mat3_t next = mat3_multiply(prev, calc_joint(joints[i], angles[i]));
+		step(angles+i, prev, tool, goal);
+		prev = mat3_multiply(prev, calc_joint(joints[i], angles[i]));
+		tool = mat3_multiply(mat3_multiply(prev, mat3_inverse(next)), tool);
 	}
 	return prev;
 }
 //combine the rotation and translation matrices
 mat3_t calc_joint(double joint, double angle)
 {
-	return mat3_multiply(mat3_rotate(angle),mat3_translate(joint));
+	return mat3_multiply(mat3_rotate(angle), mat3_translate(joint));
 }
 //compute the end effector
 mat3_t calc_end(int size, double *joints, double *angles)
 {
 	mat3_t mat = mat3_identity();
 	int i;
-	for(i = 0; i < size; i++)
-	{
-		mat3_t joint = calc_joint(joints[i],angles[i]);
-		mat = mat3_multiply(mat,joint);
+	for (i = 0; i < size; i++) {
+		mat3_t joint = calc_joint(joints[i], angles[i]);
+		mat = mat3_multiply(mat, joint);
 	}
 	return mat;
 }
@@ -126,21 +132,18 @@ mat3_t mat3_rotate(double angle)
 mat3_t mat3_multiply(mat3_t a, mat3_t b)
 {
 	mat3_t c = mat3_identity();
-	int i,j,k;
-	for(i = 0; i < 3; i++)
-	{
-		for(j = 0; j < 3; j++)
-		{
+	int i, j, k;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
 			c.m[i][j] = 0;
-			for(k = 0; k < 3; k++)
-			{
+			for (k = 0; k < 3; k++) {
 				c.m[i][j] += a.m[i][k] * b.m[k][j];
 			}
 		}
 	}
 	return c;
 }
-//multiply the [3x3] by [3x1] matrix (a column which is [0,0,1])
+//multiply the [3x3] by [3x1] matrix (a column which is [0, 0, 1])
 vec2_t mat3_getPosition(mat3_t m)
 {
 	vec2_t v;
@@ -166,13 +169,11 @@ mat3_t mat3_identity()
 //prints the matrix
 void mat3_print(mat3 mat)
 {
-	if(!mat) return;
-	int i,j;
-	for(i = 0; i < 3; i++)
-	{
-		for(j = 0; j < 3; j++)
-		{
-			printf(" %f",mat->m[i][j]);
+	if (!mat) return;
+	int i, j;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			printf(" %f", mat->m[i][j]);
 		}
 		puts(" ");
 	}
@@ -185,14 +186,13 @@ double mat2_det(double a[][2])
 //takes the determinant of the minor matrix specified by skipping row r and column c
 double mat2_det_mat3(mat3 m, int r, int c)
 {
-	int a,b,i,j;
+	int a, b, i, j;
 	double minor[2][2];
-	for(a = 0,i = 0; i < 2; a++,i++)
-	{
-		if(i == r) a++;
-		for(b = 0,j = 0; j < 2; b++,j++)
-		{
-			if(j == c) b++;
+	for (a = 0, i = 0; i < 2; a++, i++) {
+		if (i == r) a++;
+		for (b = 0, j = 0; j < 2; b++, j++) {
+			if (j == c) 
+				b++;
 			minor[i][j] = m->m[a][b];
 		}
 	}
@@ -204,15 +204,13 @@ double mat3_det(mat3 m)
 	double ret[2][2];
 	double total = 0;
 	int sign = 1;
-	int a,b,i,j,k;
-	for(i = 0; i < 3; i++)
-	{
+	int a, b, i, j, k;
+	for (i = 0; i < 3; i++) {
 		a = b = 0;
-		for(j = 1; j < 3; j++)//Does not start from 0th row
-		{
-			for(k = 0; k < 3; k++)
-			{
-				if(k==i) continue; //skip row;
+		for (j = 1; j < 3; j++) { /*Does not start from 0th row*/
+			for (k = 0; k < 3; k++) {
+				if (k == i) 
+					continue; //skip row;
 				ret[a][b++] = m->m[j][k];
 			}
 			a++;
@@ -226,21 +224,21 @@ double mat3_det(mat3 m)
 //returns a^b
 int powerInt(int a, int b)
 {
-	if(b < 1) return 1;
-	return a * powerInt(a,b-1);
+	if (b < 1) 
+		return 1;
+	return a * powerInt(a, b-1);
 }
 //takes the inverse of a 3x3 matrix
 mat3_t mat3_inverse(mat3_t m)
 {
 	double det = mat3_det(&m);
-	if(det==0) exit(-1);
-	int i,j;
+	if (det == 0)
+		exit(-1);
+	int i, j;
 	mat3_t a = mat3_identity();
-	for(i = 0; i < 3; i++)
-	{
-		for(j = 0; j < 3; j++)
-		{
-			a.m[j][i] = powerInt(-1,i+j) * mat2_det_mat3(&m,i,j) / det;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			a.m[j][i] = powerInt(-1, i+j) * mat2_det_mat3(&m, i, j) / det;
 		}
 	}
 	return a;
