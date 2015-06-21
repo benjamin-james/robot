c16968415e4824082f46dd2529a20ae1dc109955
Benjamin James <benjamin-james@utulsa.edu>
2015-06-20 23:00:40 -0500

    hardcoded values

--- src/comm.sh
+++ src/comm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Usage: $0 /dev/ttywhatever
-echo "Opening $1"
-stty -F $1 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
+# Usage: $0
+echo "Opening /dev/ttyACM0"
+stty -F /dev/ttyACM0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 printf ">"
 while read line
 do
     if [ "$line" == "exit" ] ; then
 	exit
     fi
-    echo $line > $1
-    tail -f $1
+    echo $line > /dev/ttyACM0
+    tail -f /dev/ttyACM0
     printf ">"
 done
 exit
